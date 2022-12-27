477003b85c40a8db450a92f48803a8699197ad4a
Jake Ferrari <jakeferrari93@gmail.com>
2022-12-26 21:28:21 -0800

    add partitionD script for ps

---
+++ Windows/Scripts/partitionD.ps1
@@ -0,0 +1,18 @@
+# Check if running as ADMIN
+Write-Host "Checking for elevated permissions..."
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+[Security.Principal.WindowsBuiltInRole] "Administrator")) {
+Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
+Break
+}
+else {
+Write-Host "Code is running as administrator — go on executing the script..." -ForegroundColor Green
+}
+
+# Ensure D drive letter is not taken
+Set-Partition -DriveLetter D -NewDriveLetter H
+# Create new drive partition from extra space on OS disk
+Get-Partition -DriveLetter C | Select DiskNumber
+Write-Warning "Drive D is about to be created from disk 0, is the information correct?" -WarningAction Inquire
+New-Partition -DiskNumber 0 -UseMaximumSize -DriveLetter D | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Games"
+
