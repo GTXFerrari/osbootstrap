be39ced769dd75196459773d3dd361712a4bacf9
Jake Ferrari <jakeferrari93@gmail.com>
2023-01-14 22:11:36 -0800

    rm kde & gnome files

--- Linux/Arch/Scripts/gnome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pacman -S xorg gnome gnome-extra gnome-tweaks gnome-themes-extra gdm
-systemctl enable gdm.service
--- Linux/Arch/Scripts/kde.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pacman -S xorg plasma kde-applications plasma-nm packagekit-qt5 sddm
-systemctl enable sddm
-##NOTE## Add plasma-nm to kde taskbar via Panel options > Add Widgets > Networks menu
