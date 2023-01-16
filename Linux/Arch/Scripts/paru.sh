79c1558697915e1d988ddb8aab984db8d01a32ac
Jake Ferrari <jakeferrari93@gmail.com>
2023-01-15 18:39:46 -0800

    rm dwm script, update formatting on paru & smb scripts

--- Linux/Arch/Scripts/paru.sh
+++ Linux/Arch/Scripts/paru.sh
@@ -1,45 +1,39 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 # Variables
 DIR="/home/jake/Git"
 PARU="/home/jake/Git/paru"
-
-
+echo "This script should be ran as a standard user after rebooting from the install ISO, would you like to continue? (y/n) "
+read -r usr 
+if [[ "$usr" == "n" ]]; then
+    exit
+fi
 # Create Git directory and clone paru
-if [ -d "$DIR" ]; then
-    echo "Git directory aleady exist"
-    cd $DIR
-fi
-
 if [ ! -d "$DIR" ]; then
-    echo "Git directory does not exist, Creating directory"
-    mkdir -p $DIR && cd $DIR
+    echo "Git directory does not exist, creating directory"
+    mkdir -p "$DIR" && cd "$DIR" || return
+    else
+    echo "Git directory already exists"
+    cd "$DIR" || return
 fi
-
 # Check to see if paru already exist, if not then make the PKGBUILD
-if [ -d "$PARU" ]; then
-    echo "paru directory already exist"
-fi
-
 if [ ! -d "$PARU" ]; then
-    echo "paru directory does not exist, cloning repo"
-    cd $DIR
-    git clone https://aur.archlinux.org/paru
-    cd $PARU
-    makepkg -si
+    echo "Paru does not exist, cloning repo & building"
+    cd "$DIR" && git clone https://aur.archlinux.org/paru && cd "$PARU" && makepkg -si
+    else
+    echo "Paru directory already exists, rebuilding"
+    cd "$PARU" && makepkg -si
 fi
-
 #Install packages with paru
+echo "Installing packages from the AUR"
 paru -S --needed betterdiscord-installer brave-bin cider fastfetch-git gdlauncher-bin mangohud noto-fonts-emoji-apple nsxiv ookla-speedtest-bin openrgb pfetch piavpn-bin proton-ge-custom-bin razergenie visual-studio-code-bin vmware-workstation cpu-x gwe duckstation-git pcsx2-git cemu yuzu-early-access lf goverlay-bin vkbasalt nvim-packer-git zsh-fast-syntax-highlighting nerd-fonts-jetbrains-mono zsh-theme-powerlevel10k-git rpcs3-git
-
 #BTRFS packages
-paru -S --needed timeshift timeshift-autosnap
-
+echo -n "Are you using btrfs? (y/n) "
+read -r btrfs
+if [[ "$btrfs" == "y" ]]; then
+    paru -S --needed timeshift timeshift-autosnap
+fi
 # Enable systemd services
-sudo systemctl enable --now piavpn.service
+sudo systemctl enable --now piavpn.service vmware-networks.service vmware-usbarbitrator.service
 sudo systemctl start vmware-networks-configuration.service
-sudo systemctl enable --now vmware-networks.service
-sudo systemctl enable --now vmware-usbarbitrator.service
-
 # Add user to plugdev group for razergenie
-sudo gpasswd -a jake plugdev
+sudo gpasswd -a "$USER" plugdev
--- Linux/Arch/Scripts/smb.sh
+++ Linux/Arch/Scripts/smb.sh
@@ -1,71 +1,53 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 # Variables
 DIR="/etc/samba/credentials"
 SHARE="/etc/samba/credentials/share"
 TRUENAS="/home/jake/TrueNAS"
 NAS="//10.0.40.5"
 OPT="_netdev,nofail,credentials=/etc/samba/credentials/share 0 0"
-
+echo "Downloading prerequisite packages"
+sudo pacman -S --neeeded --noconfirm cifs-utils
 # Create a TrueNAS directory with share mountpoints
-if [ -d "$TRUENAS" ]; then
-    echo "TrueNAS directory already exist"
-fi
-
 if [ ! -d "$TRUENAS" ]; then
-    echo "TrueNAS directory does not exist, creating directory with mountpoints"
-    mkdir -p $TRUENAS && cd $TRUENAS
-    mkdir -p Jake EMP Assets Media MP
-    echo "Finished creating directories"
+    echo "TrueNAS directory does not exist, creating"
+    mkdir -p "$TRUENAS" && cd "$TRUENAS" && mkdir EMP Jake MP Media
+    else
+    echo "TrueNAS directory already exists"
+    cd "$TRUENAS" && mkdir EMP Media MP Jake
 fi
-
 # Create /etc/samba/credentials directory
-if [ -d "$DIR" ]; then
-    echo "Credentials directory already exist"
-    cd $DIR
-fi
-
 if [ ! -d "$DIR" ]; then
-    echo "Credentials directory does not exist, Creating directory"
-    mkdir -p $DIR && cd $DIR
+    echo "Credentials directory does not exist, creating directory"
+    sudo mkdir -p /etc/samba/credentials
+    else
+    echo "Credentials directory already exists"
 fi
-
 # Create the share file
-if [ -d "$SHARE" ]; then
-    echo "Share file already exist"
-fi
-
-if [ ! -d "$SHARE" ]; then
+if [ ! -d "$SHARE" ]; then 
     echo "Share file does not exist, creating file"
-    touch $SHARE
-    echo "username=jake" >> $SHARE
+    sudo touch "$SHARE"
+    "username=$USER" | sudo tee -a "$SHARE" > /dev/null 
     echo -n Password:
-    read -s password
-    echo
-    echo password=$password >> $SHARE
-fi
-
+    read -r Password
+    "password=$Password" | sudo tee -a "$SHARE" > /dev/null 
+ else
+    echo "Share file already exists"
+fi    
 # Change ownership and file attributes of newly created directories and files
 if [ -d "$SHARE" ]; then
     echo "Changing permissions"
-    chown root:root $DIR
-    chmod 700 $DIR
-    chmod 600 $SHARE
+    chown root:root "$DIR" && chmod 700 "$DIR" && chmod 600 "$SHARE"
 fi
-
 # Add SMB share to fstab for automounting
-
-echo " " >> /etc/fstab
-echo "$NAS/Jake              $TRUENAS/Jake           cifs            $OPT" >> /etc/fstab
-echo " " >> /etc/fstab
-echo "$NAS/Media             $TRUENAS/Media          cifs            $OPT" >> /etc/fstab
-echo " " >> /etc/fstab
-echo "$NAS/Assets            $TRUENAS/Assets         cifs            $OPT" >> /etc/fstab
-echo " " >> /etc/fstab
-echo "$NAS/MP                $TRUENAS/MP             cifs            $OPT" >> /etc/fstab
-echo " " >> /etc/fstab
-echo "$NAS/EMP                $TRUENAS/EMP             cifs            $OPT" >> /etc/fstab
-
-systemctl daemon-reload
-mount -a
-
+{
+    echo " "
+    echo "$NAS"/Jake       "$TRUENAS"/Jake         cifs        "$OPT"
+    echo " "
+    echo "$NAS"/EMP        "$TRUENAS"/EMP          cifs        "$OPT"
+    echo " "
+    echo "$NAS"/Media      "$TRUENAS"/Media        cifs        "$OPT"
+    echo " "
+    echo "$NAS"/MP         "$TRUENAS"/MP           cifs        "$OPT"
+} >> /etc/fstab
+echo "Mounting Shares"
+systemctl daemon-reload && sudo mount -a
