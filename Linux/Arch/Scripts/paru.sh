f53a1cc63e2ff5c256c7e761722e81e156e49b66
Jake Ferrari <jakeferrari93@gmail.com>
2023-03-09 15:20:15 -0800

    move jetbrains-mono-nerd from aur to core

--- Linux/Arch/Scripts/paru.sh
+++ Linux/Arch/Scripts/paru.sh
@@ -24,7 +24,7 @@ if [ ! -d "$PARU" ]; then
 fi
 #Install packages with paru
 echo "Installing packages from the AUR"
-paru -S --needed betterdiscord-installer brave-bin cider fastfetch-git gdlauncher-bin mangohud noto-fonts-emoji-apple nsxiv ookla-speedtest-bin openrgb pfetch piavpn-bin proton-ge-custom-bin razergenie visual-studio-code-bin vmware-workstation cpu-x gwe duckstation-git pcsx2-git cemu yuzu-early-access lf goverlay-bin vkbasalt nvim-packer-git zsh-fast-syntax-highlighting nerd-fonts-jetbrains-mono zsh-theme-powerlevel10k-git rpcs3-git
+paru -S --needed betterdiscord-installer brave-bin cider fastfetch-git gdlauncher-bin mangohud noto-fonts-emoji-apple nsxiv ookla-speedtest-bin openrgb pfetch piavpn-bin proton-ge-custom-bin razergenie visual-studio-code-bin vmware-workstation cpu-x gwe duckstation-git pcsx2-git cemu yuzu-early-access lf goverlay-bin vkbasalt nvim-packer-git zsh-fast-syntax-highlighting zsh-theme-powerlevel10k-git rpcs3-git
 #BTRFS packages
 echo -n "Are you using btrfs? (y/n) "
 read -r btrfs
