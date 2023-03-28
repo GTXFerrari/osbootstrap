139d93e0e7a05cd537d00903fa5d1ac7e6cdcb3f
Jake Ferrari <jakeferrari93@gmail.com>
2023-03-28 13:53:07 -0700

    rm zramd option in paru script

--- Linux/Arch/Scripts/paru.sh
+++ Linux/Arch/Scripts/paru.sh
@@ -25,14 +25,8 @@ fi
 #Install packages with paru
 echo "Installing packages from the AUR"
 paru -S --needed betterdiscord-installer brave-bin cider fastfetch-git gdlauncher-bin mangohud noto-fonts-emoji-apple nsxiv ookla-speedtest-bin openrgb pfetch piavpn-bin proton-ge-custom-bin razergenie visual-studio-code-bin vmware-workstation cpu-x duckstation-git pcsx2-git cemu lf goverlay-bin nvim-packer-git zsh-fast-syntax-highlighting zsh-theme-powerlevel10k-git rpcs3-git adwaita-qt5 adwaita-qt6 xone-dkms
-# zramd
-echo -n "Would you like to use zramd for your swap? (y/n) "
-read -r zram
-if [[ "$zram" == "y" ]]; then
-    paru -S --needed zramd
-fi
 # Enable systemd services
-sudo systemctl enable --now piavpn.service vmware-networks.service vmware-usbarbitrator.service zramd.service
+sudo systemctl enable --now piavpn.service vmware-networks.service vmware-usbarbitrator.service
 sudo systemctl start vmware-networks-configuration.service
 # Add user to plugdev group for razergenie
 sudo gpasswd -a "$USER" plugdev
