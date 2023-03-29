d3f840a9f0305679df9daa062714569d3547e0ce
Jake Ferrari <jakeferrari93@gmail.com>
2023-03-28 20:29:52 -0700

    add nvim script

---
+++ Linux/Arch/Scripts/nvim.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed go rust luarocks ruby composer php nodejs npm python python-pip jdk-openjdk julia wget curl gzip tar bash wl-clipboard ripgrep fd 
+
+paru -S --needed powershell 
+
