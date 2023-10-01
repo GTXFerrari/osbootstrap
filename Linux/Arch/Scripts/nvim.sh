c976d404c17a7fa4049f74839003ab86476a1670
Jacob Ferrari <jakeferrari93@gmail.com>
2023-09-30 19:36:06 -0700

    Update nvim.sh
    format nvim script & add xclip for clipboard

--- Linux/Arch/Scripts/nvim.sh
+++ Linux/Arch/Scripts/nvim.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --needed go rust luarocks ruby composer php nodejs npm python python-pip jdk-openjdk julia wget curl gzip tar bash wl-clipboard ripgrep fd 
-
-paru -S --needed powershell 
-
+sudo pacman -S --needed \
+  go \
+  rust \
+  luarocks \
+  composer \
+  php \
+  nodejs \
+  npm \
+  python \
+  python-pip \
+  jdk-openjdk \
+  wget \
+  curl \
+  gzip \
+  tar \
+  bash \
+  xclip \
+  wl-clipboard \
+  ripgrep \
+  fd
