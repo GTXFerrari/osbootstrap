9b918f1ca33614dbb3ce6771d4e076c9fe921db6
Jacob Ferrari <jakeferrari93@gmail.com>
2023-10-18 21:10:25 -0700

    update the host script
    -- export colors globally,
    -- change read command to include -r to remove escape sequences
    -- encase variables in double quotes
    -- add check internet connection function and call that function at the beginning of the script
    -- create a while loop before the luks setup, if the user enters the password incorrectly they will have the ability to run cryptsetup again, prior to this the whole script would break.

--- Linux/Arch/Scripts/chroot.sh
+++ Linux/Arch/Scripts/chroot.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-# Colors
-NC='\033[0m' # No Color
-Black='\033[0;30m'
-Red='\033[0;31m'
-Green='\033[0;32m'
-Yellow='\033[0;33m'
-Blue='\033[0;34m'
-Purple='\033[0;35m'
-Cyan='\033[0;36m'
-White='\033[0;37m'
-
 init() {
   echo -e "${Green}Setting timezone.${NC}"
   ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
