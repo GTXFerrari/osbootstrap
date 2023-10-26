03dbbbec5a718e9694a4bf81eba1637edb53cc98
Jacob Ferrari <jakeferrari93@gmail.com>
2023-10-26 00:36:25 -0700

    chqange tmp to fix script, create drive variables for script

--- Linux/Arch/Scripts/fix.sh
+++ Linux/Arch/Scripts/fix.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cryptsetup open /dev/p2 cryptbtrfs
+# This script is used to chroot into the system in case you need to fix something
+cryptsetup open /dev/nvme1n1p2 cryptbtrfs
 mount_opts="rw,noatime,compress-force=zstd:1,space_cache=v2"
 mount -o ${mount_opts},subvol=@ /dev/mapper/cryptbtrfs /mnt
 mkdir -p /mnt/{home,.snapshots,var/cache,var/lib/libvirt,var/log,var/tmp}
@@ -9,4 +10,4 @@ mount -o ${mount_opts},subvol=@cache /dev/mapper/cryptbtrfs /mnt/var/cache
 mount -o ${mount_opts},subvol=@libvirt /dev/mapper/cryptbtrfs /mnt/var/lib/libvirt
 mount -o ${mount_opts},subvol=@log /dev/mapper/cryptbtrfs /mnt/var/log
 mount -o ${mount_opts},subvol=@tmp /dev/mapper/cryptbtrfs /mnt/var/tmp
-mount /dev/p1 /mnt/boot
+mount /dev/nvme1n1p1 /mnt/boot
