c2d16d0bf4787d56108564be4813b649406e3e8b
Jake Ferrari <jakeferrari93@gmail.com>
2023-11-30 00:39:34 -0800

    change mount names

--- Windows/Scripts/smb.ps1
+++ Windows/Scripts/smb.ps1
@@ -7,13 +7,13 @@ Write-Warning "Please ensure this script is running as a normal user & not admin
 # Use 'net use' to map network drives & save credentials
 Net Use Z: $NAS\Media /p:yes /savecred
 Net Use Y: $NAS\Jake /p:yes /savecred
-Net Use X: $NAS\LTS /p:yes /savecred
-Net Use W: $NAS\MP /p:yes /savecred
+Net Use X: $NAS\Stash /p:yes /savecred
+Net Use W: $NAS\Gold /p:yes /savecred
 Net Use V: $NAS\ISO /p:yes /savecred
 
 # Rename network drives
 $sh.NameSpace('Z:').Self.Name = 'Media'
 $sh.NameSpace('Y:').Self.Name = 'Jake'
-$sh.NameSpace('X:').Self.Name = 'LTS'
-$sh.NameSpace('W:').Self.Name = 'MP'
+$sh.NameSpace('X:').Self.Name = 'Stash'
+$sh.NameSpace('W:').Self.Name = 'Gold'
 $sh.NameSpace('V:').Self.Name = 'ISO'
