8d8024befba1777348090a65a53cce5fe1d8c573
Jake Ferrari <jakeferrari93@gmail.com>
2024-02-04 11:16:55 -0800

    update network shares

--- Windows/Scripts/smb.ps1
+++ Windows/Scripts/smb.ps1
@@ -8,12 +8,16 @@ Write-Warning "Please ensure this script is running as a normal user & not admin
 Net Use Z: $NAS\Media /p:yes /savecred
 Net Use Y: $NAS\Jake /p:yes /savecred
 Net Use X: $NAS\Stash /p:yes /savecred
-Net Use W: $NAS\Gold /p:yes /savecred
-Net Use V: $NAS\ISO /p:yes /savecred
+Net Use W: $NAS\Stash2 /p:yes /savecred
+Net Use V: $NAS\Gold /p:yes /savecred
+Net Use U: $NAS\ISO /p:yes /savecred
+Net Use T: $NAS\Photos /p:yes /savecred
 
 # Rename network drives
 $sh.NameSpace('Z:').Self.Name = 'Media'
 $sh.NameSpace('Y:').Self.Name = 'Jake'
 $sh.NameSpace('X:').Self.Name = 'Stash'
-$sh.NameSpace('W:').Self.Name = 'Gold'
-$sh.NameSpace('V:').Self.Name = 'ISO'
+$sh.NameSpace('W:').Self.Name = 'Stash2'
+$sh.NameSpace('V:').Self.Name = 'Gold'
+$sh.NameSpace('U:').Self.Name = 'ISO'
+$sh.NameSpace('T:').Self.Name = 'Photos'
