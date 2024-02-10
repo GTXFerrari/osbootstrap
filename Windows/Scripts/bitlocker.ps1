f424f40619d634534dd5311c9c9ecadd63379807
Jake Ferrari <jakeferrari93@gmail.com>
2024-02-10 14:12:51 -0800

    clean up bitlocker script

--- Windows/Scripts/bitlocker.ps1
+++ Windows/Scripts/bitlocker.ps1
@@ -1,8 +1,10 @@
-# Variables
+# Used for Boot drive
 $UsrPin = Read-Host -Prompt 'Input your bitlocker pin'
-$UsrPass = Read-Host -Prompt 'Input your bitlocker password'
 $Pin = ConvertTo-SecureString $UsrPin -AsPlainText -Force
-$Pass = ConvertTo-SecureString $UsrPass -AsPlainText -Force
+
+# Used for data drive
+#$UsrPass = Read-Host -Prompt 'Input your bitlocker password'
+#$Pass = ConvertTo-SecureString $UsrPass -AsPlainText -Force
 
 Write-Warning "This script will enable bitlocker on the OS Drive & a data drive with the mount point D:. TPM+PIN also requires a custom group policy. Would you like to proceed?" -WarningAction Inquire 
 # Check if running as ADMIN
@@ -16,12 +18,14 @@ else {
 Write-Host "Code is running as administrator — go on executing the script..." -ForegroundColor Green
 }
 
-# Enable Bitlocker
+# Enable Bitlocker for Boot
 Enable-BitLocker -MountPoint "C" -EncryptionMethod XtsAes256 -Pin $Pin -TpmAndPinProtector  # Requires a custom group policy to enable PIN
 # Enable additional recovery keys
 Add-BitLockerKeyProtector -MountPoint "C" -RecoveryPasswordProtector   # Store the generated recoverypassword in a safe location
 manage-bde -protectors -get C: > '\\10.0.40.5\Jake\Backups\Bitlocker\Jakes-PC Bitlocker Recovery Codes\C_Drive\BLRecoveryKey.txt'
+
 # Enable bitlocker for Data drives (D:)
 #Enable-BitLocker -MountPoint "D:" -EncryptionMethod XtsAes256 -PasswordProtector -Password $Pass
 #Enable-BitLockerAutoUnlock -MountPoint "D:"
+
 Restart-Computer -Confirm
