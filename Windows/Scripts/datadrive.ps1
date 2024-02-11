23e95ed0e3b21c573677d484cf070bcfffa56bea
Jake Ferrari <jakeferrari93@gmail.com>
2024-02-10 22:53:32 -0800

    - Get disk num for C drive and store as var to create new partition on same disk - Add bios location to emu dir - Clean up robocopy to use UNC addresses instead of mapped drive since script is ran as admin - Add bitlocker script for data drive with autounlock option

--- Windows/Scripts/datadrive.ps1
+++ Windows/Scripts/datadrive.ps1
@@ -9,6 +9,10 @@ else {
 Write-Host "Code is running as administrator — go on executing the script..." -ForegroundColor Green
 }
 
+# Bitlocker for data drive
+$UsrPass = Read-Host -Prompt 'Input your bitlocker password'
+$Pass = ConvertTo-SecureString $UsrPass -AsPlainText -Force
+
 # Ensure D drive letter is not taken
 Set-Partition -DriveLetter D -NewDriveLetter H
 Get-Partition -DriveLetter C | Select-Object DiskNumber
@@ -16,7 +20,8 @@ Start-Sleep 3
 
 # Create new drive partition from extra space on OS disk
 Write-Warning "Drive D is about to be created from disk 0, is the information correct?" -WarningAction Inquire
-New-Partition -DiskNumber 0 -UseMaximumSize -DriveLetter D | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Data"
+$cDriveDiskNumber = (Get-Partition -DriveLetter C).DiskNumber
+New-Partition -DiskNumber $cDriveDiskNumber -UseMaximumSize -DriveLetter D | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Data"
 
 # Create directories in new partition
 Set-Location -LiteralPath D:
@@ -24,7 +29,7 @@ New-Item -ItemType Directory VM,Games,Code,WSL
 Set-Location -LiteralPath D:\Games
 New-Item -ItemType Directory Steam,EA,Blizzard,Emulator,Ubisoft
 Set-Location -LiteralPath D:\Games\Emulator
-New-Item -ItemType Directory Nintendo,Microsoft,Sony,Retroarch 
+New-Item -ItemType Directory Nintendo,Microsoft,Sony,Retroarch,BIOS 
 Set-Location -LiteralPath D:\Games\Emulator\Nintendo
 New-Item -ItemType Directory Switch,'Wii U'
 New-Item -ItemType Directory Switch\Titles
@@ -40,6 +45,13 @@ Set-Location -LiteralPath D:\VM
 New-Item -ItemType Directory VMware,ISO
 
 # Move Game files to location 
-#robocopy /V /ETA /E Z:\Games\Nintendo\Switch\ D:\Games\Emulator\Nintendo\Switch\Titles\
-#robocopy /V /ETA /E 'Z:\Games\Nintendo\Wii U\BOTW' 'D:\Games\Emulator\Nintendo\Wii U\Titles\'
-#robocopy /V /ETA /E 'Z:\Games\Sony\Playstation 3\' 'D:\Games\Emulator\Sony\Playstation 3\Titles\'
+robocopy /V /ETA /E '\\10.0.40.5\Media\Games\System BIOS\' 'D:\Games\Emulator\BIOS\'
+robocopy /V /ETA /E '\\10.0.40.5\Media\Games\Nintendo\Switch\' 'D:\Games\Emulator\Nintendo\Switch\Titles\'
+robocopy /V /ETA /E '\\10.0.40.5\Media\Games\Nintendo\Wii U\' 'D:\Games\Emulator\Nintendo\Wii U\Titles\'
+robocopy /V /ETA /E '\\10.0.40.5\Media\Games\Sony\Playstation 3\' 'D:\Games\Emulator\Sony\Playstation 3\Titles\'
+
+# Enable bitlocker for Data drives (D:)
+Enable-BitLocker -MountPoint "D:" -EncryptionMethod XtsAes256 -PasswordProtector -Password $Pass
+Enable-BitLockerAutoUnlock -MountPoint "D:"
+
+Restart-Computer -Confirm
