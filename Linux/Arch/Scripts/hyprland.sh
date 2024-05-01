4ea7697fafcca5f0587d11280375fb5a858dc66c
Jacob Ferrari <jakeferrari93@gmail.com>
2024-05-01 15:48:00 -0700

    add hyprland script

---
+++ Linux/Arch/Scripts/hyprland.sh
@@ -0,0 +1,17 @@
+sudo pacman -S --needed \
+	hyprland \
+	qt5-wayland \
+	qt6-wayland \
+	qt5ct \
+	qt6ct \
+	libva \
+	kitty \
+	xdg-desktop-portal-hyprland \
+	nwg-look \
+	polkit-kde-agent \
+	waybar \
+	wofi \
+	pavucontrol \
+	swaync \
+	gvfs-smb \
+
