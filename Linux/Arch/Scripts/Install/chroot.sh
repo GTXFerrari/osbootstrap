f6a0ee9f30efefb0fe897282d531b14cc4806c24
Jake Ferrari <jakeferrari93@gmail.com>
2024-08-22 19:05:47 -0700

    mv individual scripts to prev dir

--- Linux/Arch/Scripts/Install/chroot.sh
+++ Linux/Arch/Scripts/Install/chroot.sh
@@ -15,8 +15,6 @@ init() {
   echo -e "${Green}Enabling multilib.${NC}"
   sleep 1
   sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-  echo -e "${Green}Updating mirrorlist.${NC}"
-  reflector -c 'United States' -a 12 -p https --sort rate --save /etc/pacman.d/mirrorlist
   pacman -Sy
   echo -e "${Green}Updating keyring.${NC}"
   sleep 1
