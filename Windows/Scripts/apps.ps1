bc0678a218a6418bf3bc08f97249570c5088a116
Jacob Ferrari <jakeferrari93@gmail.com>
2024-09-02 19:25:58 -0700

    Change write-output to write-host
    Less overhead with write-host

--- Windows/Scripts/apps.ps1
+++ Windows/Scripts/apps.ps1
@@ -11,7 +11,7 @@ if ($PSVersionTable.PSVersion.Major -lt 7)
 }
 
 # Check if running as ADMIN
-Write-Output "Checking for elevated permissions..."
+Write-Host "Checking for elevated permissions..."
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
 [Security.Principal.WindowsBuiltInRole] "Administrator")) {
 Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
