80bab7446a7e51548709f2a5162eea195c2f51df
Jacob Ferrari <jakeferrari93@gmail.com>
2024-09-02 19:26:19 -0700

    Update admin check
    Only write-host if admin priv is not met

--- Windows/Scripts/apps.ps1
+++ Windows/Scripts/apps.ps1
@@ -13,12 +13,10 @@ if ($PSVersionTable.PSVersion.Major -lt 7)
 # Check if running as ADMIN
 Write-Host "Checking for elevated permissions..."
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-[Security.Principal.WindowsBuiltInRole] "Administrator")) {
-Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
-Break
-}
-else {
-Write-Output "Code is running as administrator - go on executing the script..."
+            [Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
+    Break
 }
 
 # Install applications using winget
