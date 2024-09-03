5b1d8ce75e635ef8368a536fa4b7ac94be8e686e
Jacob Ferrari <jakeferrari93@gmail.com>
2024-09-02 21:07:12 -0700

    Refactor admin check

--- Windows/Scripts/datadrive.ps1
+++ Windows/Scripts/datadrive.ps1
@@ -1,12 +1,13 @@
 # Check if running as ADMIN
 Write-Host "Checking for elevated permissions..."
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-[Security.Principal.WindowsBuiltInRole] "Administrator")) {
-Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
-Break
-}
-else {
-Write-Host "Code is running as administrator — go on executing the script..." -ForegroundColor Green
+            [Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "Insufficient permissions to run this script. Open the PowerShell console as an administrator and run this script again."
+    Break
+} else
+{
+    Write-Host "Code is running as administrator — go on executing the script..." -ForegroundColor Green
 }
 
 # Bitlocker for data drive
