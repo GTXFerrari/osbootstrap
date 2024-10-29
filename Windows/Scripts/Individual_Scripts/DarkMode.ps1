dbb4c97ddce996939594cacea0719f7ed2622f4b
Jake Ferrari <jakeferrari93@gmail.com>
2024-10-28 18:28:15 -0700

    Remove dark mode script

--- Windows/Scripts/Individual_Scripts/DarkMode.ps1
+++
@@ -1,3 +0,0 @@
-# Enable Dark-Mode (VM)
-Set-ItemProperty -Path REGISTRY::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0
-Set-ItemProperty -Path REGISTRY::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
