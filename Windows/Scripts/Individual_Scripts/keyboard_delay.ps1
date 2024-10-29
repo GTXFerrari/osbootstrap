344cf123848e7bf223ce8a89b2f3e495bdba5837
Jake Ferrari <jakeferrari93@gmail.com>
2024-10-28 18:19:55 -0700

    Update key repeat script
    - Lower the amount of repeat rate for key presses

--- Windows/Scripts/Individual_Scripts/keyboard_delay.ps1
+++ Windows/Scripts/Individual_Scripts/keyboard_delay.ps1
@@ -1,7 +1,7 @@
 Set-Location "HKCU:\Control Panel\Accessibility\Keyboard Response"
 
 Set-ItemProperty -Path . -Name AutoRepeatDelay       -Value 200
-Set-ItemProperty -Path . -Name AutoRepeatRate        -Value 16
+Set-ItemProperty -Path . -Name AutoRepeatRate        -Value 12
 Set-ItemProperty -Path . -Name DelayBeforeAcceptance -Value 0
 Set-ItemProperty -Path . -Name BounceTime            -Value 0
 Set-ItemProperty -Path . -Name Flags                 -Value 47
