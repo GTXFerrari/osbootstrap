72dae58587e0c872c837c49b2ceb705fe91423d4
Jake Ferrari <jakeferrari93@gmail.com>
2024-11-09 12:57:26 -0800

    Update hyprland install script
    - Update packages for hyprland
    - Remove git_dir var and git clone to desired dir

--- Linux/Arch/Scripts/Individual_Scripts/WIP/hyprland_install.sh
+++ Linux/Arch/Scripts/Individual_Scripts/WIP/hyprland_install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-git_dir=$HOME/Git
-
-sudo pacman -S --needed hyprland hypridle hyprlock kitty nwg-look wofi swww xdg-desktop-portal-hyprland grim slurp ttf-font-awesome otf-font-awesome lf nautilus polkit-gnome swayimg swaync
+sudo pacman -S --needed hyprland xdg-desktop-portal-hyprland hypridle hyprlock nwg-look wofi swww grim slurp ttf-font-awesome otf-font-awesome lf swayimg kitty
 
 # Hyprpanel Setup
 curl -fsSL https://bun.sh/install | bash &&
@@ -11,5 +9,5 @@ curl -fsSL https://bun.sh/install | bash &&
 sudo pacman -S pipewire libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon
 yay -S grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat aylurs-gtk-shell-git
 # Installs HyprPanel to ~/.config/ags
-cd "$git_dir" && git clone https://github.com/Jas-SinghFSU/HyprPanel.git &&
-  ln -s "$(pwd)"/HyprPanel "$HOME"/.config/ags
+git clone https://github.com/Jas-SinghFSU/HyprPanel.git ~/Git/HyprPanel
+ln -s "$(pwd)"/HyprPanel "$HOME"/.config/ags
