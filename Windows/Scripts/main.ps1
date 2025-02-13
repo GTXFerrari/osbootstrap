ef5a0de5f0864c6e33198f4ede8ab0ea187ac4bc
Jacob Ferrari <jakeferrari93@gmail.com>
2025-02-12 20:19:01 -0800

    Update ssh_setup
    - Change ssh_setup name to sshd_setup
    - Add openssh client & server installs
    - Change service name to sshd from ssh

--- Windows/Scripts/main.ps1
+++ Windows/Scripts/main.ps1
@@ -104,15 +104,17 @@ function power_options {
     }
 }
 
-function ssh_setup {
+function sshd_setup
+{
   $ssh_setup = Read-Host "Would you like to setup ssh on the host? (Y/N)"
-    if ($ssh_setup -eq "Y" -or $ssh_setup -eq "y")
-    {
-        New-Item -ItemType Directory $env:USERPROFILE\.ssh\
-        Get-Service ssh-agent | Set-Service -StartupType Automatic
-        Start-Service ssh-agent
-        Get-Service ssh-agent
-    }
+  if ($ssh_setup -eq "Y" -or $ssh_setup -eq "y")
+  {
+    Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    New-Item -ItemType Directory $Env:USERPROFILE\.ssh\
+    Start-Service sshd
+    Set-Service -Name sshd -StartupType 'Automatic'
+  }
 }
 
 function rename_pc {
