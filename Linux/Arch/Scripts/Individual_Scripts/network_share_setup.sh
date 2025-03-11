fd430efa1ba1897452d406d0692c7a2a34143b10
Jake Ferrari <jakeferrari93@gmail.com>
2025-03-10 19:04:53 -0700

    Update network share script
    - Create new share (gold)
    - Add sudo to commands that need priv elevation
    - Add conditional to download the right package depending on distro
    (debian & arch only)

--- Linux/Arch/Scripts/Individual_Scripts/network_share_setup.sh
+++ Linux/Arch/Scripts/Individual_Scripts/network_share_setup.sh
@@ -6,43 +6,37 @@ truenas_dir="/mnt/truenas"
 nas_addr="//10.0.40.5"
 smb_options="_netdev,nofail,file_mode=0777,dir_mode=0777,credentials=/etc/samba/credentials/share 0 0"
 
-if [[ "$(id -u)" -ne 0 ]]; then
-  echo "Please run the script using sudo or as root!"
-fi
-
 # Check dependencies
 echo "Checking script dependencies"
 sleep 2
-while true; do
-  if pacman -Qi gum >/dev/null 2>&1; then
-    break
-  else
-    pacman -S --noconfirm gum cifs-utils
-  fi
-done
+if [[ -f /etc/debian_version ]]; then
+  sudo apt update && sudo apt install cifs-utils
+elif [[ -f /etc/arch_release ]]; then
+  sudo pacman -Sy && sudo pacman -S --noconfirm gum cifs-utils
+fi
 
 gum style --foreground="#00ff28" --bold "Setting Up SMB Shares"
 
 if [[ ! -d "$cred_dir" ]]; then
-  mkdir -p $cred_dir
+  sudo mkdir -p $cred_dir
 fi
 
 if [[ -e $share_file ]]; then
   gum style --foreground="#00ff28" --bold "Share file already exists"
 else
-  touch $share_file
+  sudo touch $share_file
 fi
 
 smb_user=$(gum input --placeholder="Enter SMB Username")
-echo "username=$smb_user" | tee -a "$share_file" >/dev/null
+echo "username=$smb_user" | sudo tee -a "$share_file" >/dev/null
 smb_pass=$(gum input --placeholder="Enter SMB Password")
-echo "password=$smb_pass" | tee -a "$share_file" >/dev/null
+echo "password=$smb_pass" | sudo tee -a "$share_file" >/dev/null
 gum style --foreground="#00ff28" --bold "Updating permissions..."
 sleep 2
-chown root:root "$cred_dir" && chmod 700 "$cred_dir" && chmod 600 "$share_file"
+sudo chown root:root "$cred_dir" && sudo chmod 700 "$cred_dir" && sudo chmod 600 "$share_file"
 
 if [[ ! -d $truenas_dir ]]; then
-  mkdir -p /mnt/truenas/{media,stash,jake}
+  sudo mkdir -p /mnt/truenas/{media,stash,jake,gold}
 fi
 
 {
@@ -52,5 +46,9 @@ fi
   echo "$nas_addr"/Stash "$truenas_dir"/stash cifs "$smb_options"
   echo " "
   echo "$nas_addr"/Media "$truenas_dir"/media cifs "$smb_options"
+  echo " "
+  echo "$nas_addr"/Gold "$truenas_dir"/gold cifs "$smb_options"
 
-} | tee -a /etc/fstab >/dev/null
+} | sudo tee -a /etc/fstab >/dev/null
+
+sudo systemctl daemon-reload && sudo mount -a
